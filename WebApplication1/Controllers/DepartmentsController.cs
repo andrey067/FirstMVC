b91836599d3e6a84c98d76fd2214da824f8e300d
andrey067 <audrey.ernesto.lima@gmail.com>
2020-09-08 10:08:24 -0400

    First Model-Controller-View-Department

---
+++ WebApplication1/Controllers/DepartmentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        
+        
+        
+        
+        
+        
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "Eletronics" });
+            list.Add(new Departament { Id = 2, Name = "Fashion" });
+            return View(list);
+        }
+    }
+}
